60cb23be7609f9326ae1f9ed43f6401f0ab54d35
Yuliani Espitia <yuliani.espitia@e-capture.co>
2022-09-13 02:27:42 -0500

    estilos temperatura

---
+++ js/triangulos.js
@@ -0,0 +1,13 @@
+/*function lados(res){
+    var lado1=prompt('Digite el primer lado del triangulo')
+    var lado2=prompt('Digite el segundo lado del triangulo')
+    var lado3=prompt('Digite el tercer lado del triangulo')
+     res= lado1!= lado2!= lado3
+     alert("el triangulo es quilatero ya que sus lados son: " +res);
+  }*/
+
+  function mayuscula(digita){
+    var digita=prompt('Digite la palabra en minusculas que desea cambiar a mayusculas');
+    may= digita.toUpperCase();
+    alert("Su palabra en mayusculas es:" +may);
+}
