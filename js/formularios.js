731019e4cbbbb1859818a4186452109379336d6c
Yuliani Espitia <yuliani.espitia@e-capture.co>
2022-09-18 22:44:26 -0500

    Formulario

---
+++ js/formularios.js
@@ -0,0 +1,6 @@
+const validacion={
+    Text:/^[a-zA-ZÁ-ÿ\s]{1,20}$/,
+    Number:/^\d{8,10}$/,
+    email:/^[a-zA-Z0-9+-.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+    contraseña:/^(?=.+\d)(?=.*[#$%&!@])(?=.*[a-z])(?=.*[A-Z]).{5,}$/
+}
