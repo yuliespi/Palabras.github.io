e0a070d485d4ade908b3c6dedc293248a13c64b9
Yuliani Espitia <yuliani.espitia@e-capture.co>
2022-10-04 15:50:01 -0500

    Form modal

--- js/formularios.js
+++ js/formularios.js
@@ -1,3 +1,24 @@
+let btn =document.getElementById("btn");
+btn.onclick = CuadradoArea;
+
+const openElsTA = document.querySelectorAll("[data-open]");
+const isVisibleF = "is-visible";
+ 
+for(const el of openEls) {
+  el.addEventListener("click", function() {
+    const modalId = this.dataset.open;
+    document.getElementById(modalId).classList.add(isVisible);
+  });
+}
+
+const closeElsTA = document.querySelectorAll("[data-close]");
+ 
+for (const el of closeEls) {
+  el.addEventListener("click", function() {
+    this.parentElement.parentElement.parentElement.classList.remove(isVisible);
+  });
+}
+
 const validacion={
     Text:/^[a-zA-ZÁ-ÿ\s]{1,20}$/,
     Number:/^\d{8,10}$/,
