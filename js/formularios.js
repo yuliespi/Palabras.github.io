dc120bb6f8475376e7d9aea7973f8f95cd338ca7
Yuliani Espitia <yuliani.espitia@e-capture.co>
2022-10-05 15:31:57 -0500

    Form modal

--- js/formularios.js
+++ js/formularios.js
@@ -1,8 +1,12 @@
-let btn =document.getElementById("btn");
-btn.onclick = CuadradoArea;
+const validacion={
+  Text:/^[a-zA-ZÁ-ÿ\s]{1,20}$/,
+  Number:/^\d{8,10}$/,
+  email:/^[a-zA-Z0-9+-.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+  contraseña:/^(?=.+\d)(?=.*[#$%&!@])(?=.*[a-z])(?=.*[A-Z]).{5,}$/
+}
 
-const openElsTA = document.querySelectorAll("[data-open]");
-const isVisibleF = "is-visible";
+const openEls = document.querySelectorAll("[data-open]");
+const isVisible = "is-visible";
  
 for(const el of openEls) {
   el.addEventListener("click", function() {
@@ -11,17 +15,10 @@ for(const el of openEls) {
   });
 }
 
-const closeElsTA = document.querySelectorAll("[data-close]");
+const closeEls = document.querySelectorAll("[data-close]");
  
 for (const el of closeEls) {
   el.addEventListener("click", function() {
     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
   });
-}
-
-const validacion={
-    Text:/^[a-zA-ZÁ-ÿ\s]{1,20}$/,
-    Number:/^\d{8,10}$/,
-    email:/^[a-zA-Z0-9+-.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-    contraseña:/^(?=.+\d)(?=.*[#$%&!@])(?=.*[a-z])(?=.*[A-Z]).{5,}$/
 }
